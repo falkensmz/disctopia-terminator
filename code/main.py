0b1f56901ff0064839540a31276359bdda24d012
Dimitris Kalopisis <74763463+3ct0s@users.noreply.github.com>
2024-02-10 15:54:57 +0100

    Made code cleaner :)

--- code/main.py
+++ code/main.py
@@ -1,114 +1,70 @@
 import os
 import sys
-import subprocess as sp
+import subprocess
 from shutil import rmtree
 
 header = """
-    ____  _           __              _                 
-   / __ \(_)_________/ /_____  ____  (_)___ _           
-  / / / / / ___/ ___/ __/ __ \/ __ \/ / __ `/           
- / /_/ / (__  ) /__/ /_/ /_/ / /_/ / / /_/ /            
-/_____/_/____/\___/\__/\____/ .___/_/\__,_/             
-  ______                   /_/            __            
- /_  __/__  _________ ___  (_)___  ____ _/ /_____  _____
-  / / / _ \/ ___/ __ `__ \/ / __ \/ __ `/ __/ __ \/ ___/
- / / /  __/ /  / / / / / / / / / / /_/ / /_/ /_/ / /    
-/_/  \___/_/  /_/ /_/ /_/_/_/ /_/\__,_/\__/\____/_/     
-                                                        
-                                                                                                                                                                                    
-Version: 1.0.0                                                                               
+______           _              _         _                      _             _             
+|  _  \         | |            (_)       | |                    (_)           | |            
+| | | |_   _ ___| |_ ___  _ __  _  __ _  | |_ ___ _ __ _ __ ___  _ _ __   __ _| |_ ___  _ __ 
+| | | | | | / __| __/ _ \| '_ \| |/ _` | | __/ _ \ '__| '_ ` _ \| | '_ \ / _` | __/ _ \| '__|
+| |/ /| |_| \__ \ || (_) | |_) | | (_| | | ||  __/ |  | | | | | | | | | | (_| | || (_) | |   
+|___/  \__, |___/\__\___/| .__/|_|\__,_|  \__\___|_|  |_| |_| |_|_|_| |_|\__,_|\__\___/|_|   
+        __/ |            | |                                                                 
+       |___/             |_|                                                                 
 """
+
+def run_command(command):
+    """Executes a shell command and returns its output and error."""
+    result = subprocess.run(command, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True, text=True)
+    return result.stdout, result.stderr
 
 def check_registry():
     print("[+] Checking for registry key...")
-    key = r'HKCU\Software\Microsoft\Windows\CurrentVersion\Run\update'
-    result = sp.Popen(f"reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v update", stderr=sp.PIPE, stdin=sp.DEVNULL, stdout=sp.PIPE, shell=True, text=True, creationflags=0x08000000)
-    out, err = result.communicate()
-    result.wait()
-    if err:
-        return False
-    else:
-        return True
+    out, err = run_command("reg query HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v update")
+    return not err
 
 def check_config():
     print("[+] Checking for config file...")
-    USERNAME = os.environ.get("USERNAME")
-    if os.path.exists(fr'C:\Users\{USERNAME}\.config'):
-        return True
-    else:
-        return False
+    config_path = os.path.join(os.environ.get("USERPROFILE"), ".config")
+    return os.path.exists(config_path)
 
-def check_peristence():
+def check_persistence():
     print("[+] Checking for persistence...\n")
-    if os.path.exists(os.environ["appdata"] + "\\Windows-Updater.exe"):
-        return True
-    else:
-        return False
+    updater_path = os.path.join(os.environ.get("APPDATA"), "Windows-Updater.exe")
+    return os.path.exists(updater_path)
 
 def check_processes():
-    print(f"[+] Checking for processes...")
-    result = sp.Popen(f"tasklist", stderr=sp.PIPE, stdin=sp.DEVNULL, stdout=sp.PIPE, shell=True, text=True, creationflags=0x08000000)
-    out, err = result.communicate()
-    result.wait()
-    if "Windows-Updater.exe" in out:
-        return True
-    else:
-        return False
+    print("[+] Checking for processes...")
+    out, err = run_command("tasklist")
+    return "Windows-Updater.exe" in out
 
 print(header)
 
-registry = check_registry()
-config = check_config()
-processes = check_processes()
-persistence = check_peristence()
+checks = [check_registry(), check_config(), check_persistence(), check_processes()]
 
-if registry or config or processes or persistence:
+if any(checks):
     print("[!] Disctopia-C2 has been detected on the system.\n")
-    choice = input("[?] Do you want to permanently delete Disctopia-c2 from the system? (Y/N): ")
-    if choice == "Y":
+    if input("[?] Do you want to permanently delete Disctopia-C2 from the system? (Y/N): ").upper() == "Y":
         print("\n[+] Deleting Disctopia-C2 from the system...")
+        if checks[0]:  # Registry
+            subprocess.call('reg delete HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v update /f', shell=True)
 
-        USERNAME = os.environ.get("USERNAME")
+        if checks[1]:  # Config
+            rmtree(os.path.join(os.environ.get("USERPROFILE"), ".config"))
 
-        if registry:
-            try:
-                sp.call('reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v update /f', shell=True)
-            except Exception as e:
-                print(f"An error has occured : {e}")
+        if checks[2]:  # Persistence
+            os.remove(os.path.join(os.environ.get("APPDATA"), "Windows-Updater.exe"))
 
-        if config:
-            try:
-                rmtree(fr'C:\Users\{USERNAME}\.config')
-            except Exception as e:
-                print(f"An error has occured : {e}")
-
-        if persistence:
-            try:
-                os.remove(os.environ["appdata"] + "\\Windows-Updater.exe")
-            except Exception as e:
-                print(f"An error has occured : {e}")
-
-        if processes:
-            try:
-                sp.Popen(f"taskkill /IM Windows-Updater.exe ", stderr=sp.PIPE, stdin=sp.DEVNULL, stdout=sp.PIPE, shell=True, text=True, creationflags=0x08000000)
-            except Exception as e:
-                print(f"An error has occured : {e}")
+        if checks[3]:  # Processes
+            subprocess.run("taskkill /IM Windows-Updater.exe /F", shell=True)
 
         print("\n[!] Threat has been neutralized successfully.")
-        print("[+] Thank you for using disctopia-terminator\n")
-        input("[ENTER] to exit")
-        sys.exit()
-
-    elif choice == "N":
-        print("[+] Thank you for using disctopia-terminator\n")
-        input("[ENTER] to exit")
-        sys.exit()
     else:
-        print("[!] Invalid input\n", 'red')
-        input("[ENTER] to exit")
-        sys.exit()
+        print("[+] Exiting without changes.")
 else:
     print("[!] Disctopia-C2 has not been detected on the system.")
-    print("Thank you for using disctopia-terminator\n")
-    input("[ENTER] to exit")
-    sys.exit()
+
+print("Thank you for using disctopia-terminator\n")
+input("[ENTER] to exit")
+sys.exit()
